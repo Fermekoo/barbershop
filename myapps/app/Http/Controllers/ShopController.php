f6ad65a6249eaea701de6a6e6858735e9a515cd5
Fermekoo <dandifermeko@gmail.com>
2018-10-24 10:45:34 +0700

    add shop

--- myapps/app/Http/Controllers/ShopController.php
+++ myapps/app/Http/Controllers/ShopController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Shop;
+use DB;
+use Validator;
+use Auth;
 class ShopController extends Controller
 {
-    //
+    public function add(Request $req){
+         $validator = Validator::make($req->all(), 
+            [
+                 'nama_barber' => 'required',
+                 'longitude' => 'required',
+                 'latitude'=>'required'
+            ]
+        );
+        if ($validator->fails()) {
+            $data = array();
+            foreach($validator->messages()->getMessages() as $field_name => $values) {
+                $data[$field_name]  = $values[0];
+            }
+            $message = 'Register Failed';
+            return response()->json(['code' => 401, 'status' => 'Failed', 'message' => $message, 'data' => $data],400);
+        }
+        DB::beginTransaction();
+        try{
+            $save = Shop::create([
+                'nama_barber'=>$req->nama_barber,
+                'longitude'=>$req->longitude,
+                'latitude'=>$req->latitude,
+                'status'=>1
+            ]);
+        }catch(Exception $e){
+            DB::rollBack();
+        }
+        if($save){
+            DB::commit();
+            return response()->json(['code'=>200,'status'=>'success','message'=>'data berhasil disimpan'],201);
+        }else{
+            return response()->json(['code'=>401,'status'=>'failed','message'=>'data gagal disimpan'],400);
+        }
+        
+
+    }
 }
